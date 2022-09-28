42125dd06d6eb1c28dca8154bd6bfe4276baec90
remon2207 <reimond2207@gmail.com>
2022-09-29 08:44:33 +0900

    feat: Init index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+);
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+app.listen(port);
